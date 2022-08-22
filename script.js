cb9306bfad34bc908ecabad85f0999c7b1b990e0
Progamming-hub <108167720+Progamming-hub@users.noreply.github.com>
2022-08-22 19:35:44 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+/*============ typing animation  ===== */
+var typed = new Typed(".typing", {
+    strings:["Web Designer","Web Developer","Graphic Designer","Youtuber"],
+    typeSpeed:100,
+    BackSpeed:80,
+    loop:true
+})
+
+var typed = new Typed(".typing1", {
+    strings:["Web Designer","Web Developer","Graphic Designer","Youtuber"],
+    typeSpeed:100,
+    BackSpeed:80,
+    loop:true
+})
